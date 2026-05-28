3d19b03ddaa5d7be17b97508e31f93a4c83af01c
JulianoMXM <juliano.massocalegal@gmail.com>
2026-05-28 16:52:19 -0300

    refact: Altera a pasta e nome do arquivo

--- src/main/java/com/example/Almoxarifado/common/StatusEnum.java
+++
@@ -1,5 +0,0 @@
-package com.example.Almoxarifado.common;
-
-public enum StatusEnum {
-    
-}
---
+++ src/main/java/com/example/Almoxarifado/common/enums/StatusChaveEnum.java
@@ -0,0 +1,6 @@
+package com.example.Almoxarifado.common.enums;
+public enum StatusChaveEnum {
+    DISPONÍVEL,
+    EMPRESTADA,
+    DESATIVADA
+}
