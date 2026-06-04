8545816de92f282030d40d8052e8dd2e0274faac
JulianoMXM <juliano.massocalegal@gmail.com>
2026-06-04 15:06:19 -0300

    feat: Adiciona classe de componente

---
+++ src/main/java/com/example/Almoxarifado/model/Componente.java
@@ -0,0 +1,28 @@
+package com.example.Almoxarifado.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "componentes")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Getter
+@Setter
+public abstract class Componente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "A quantidade de itens é obrigatória.")
+    @PositiveOrZero(message = "A quantidade de itens não pode ser negativa")
+    private int qntDisponivel;
+
+    @NotBlank(message = "O modelo é obrigatório.")
+    private String modelo;
+
+    @NotBlank(message = "A descrição é obrigatória.")
+    private String descricao;
+}
