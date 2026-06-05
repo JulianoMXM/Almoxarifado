6a7cde594dcfbb8e81cd49f1dd9a6377830a8635
JulianoMXM <juliano.massocalegal@gmail.com>
2026-06-04 21:52:52 -0300

    feat: Adiciona controller do indutor e a DTO de atualização.

---
+++ src/main/java/com/example/Almoxarifado/controller/IndutorController.java
@@ -0,0 +1,49 @@
+package com.example.Almoxarifado.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Almoxarifado.dto.AtualizarIndutorDTO;
+import com.example.Almoxarifado.model.Indutor;
+import com.example.Almoxarifado.repository.IndutorRepository;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/indutor")
+public class IndutorController {
+    @Autowired
+    private IndutorRepository repository;
+
+    @PostMapping
+    public Indutor cadastrarIndutor(@Valid @RequestBody Indutor novoIndutor){
+        return repository.save(novoIndutor);
+    }
+
+    @PatchMapping("/{id}")
+    public Indutor atualizarIndutor(@Valid @RequestBody AtualizarIndutorDTO dto, @PathVariable Long id) throws NotFoundException{
+        Indutor indutorExistente = repository.findById(id).orElseThrow(() -> new NotFoundException());
+
+        if(dto.getIndutancia() != null){
+            indutorExistente.setIndutancia(dto.getIndutancia());
+        }
+        if(dto.getCorrenteMaxima() != null){
+            indutorExistente.setCorrenteMaxima(dto.getCorrenteMaxima());
+        }
+        if(dto.getTolerancia() != null){
+            indutorExistente.setTolerancia(dto.getTolerancia());
+        }
+        if(dto.getUnidadeDeMedida() != null){
+            indutorExistente.setUnidadeDeMedida(dto.getUnidadeDeMedida());
+        }
+        return repository.save(indutorExistente);
+    }
+}
