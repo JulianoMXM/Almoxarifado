31d6c37bd1269f637de2f9a41f4ec620407d8296
JulianoMXM <juliano.massocalegal@gmail.com>
2026-06-04 21:47:57 -0300

    feat: Adiciona diferença de rotas.

--- src/main/java/com/example/Almoxarifado/controller/DiodoController.java
+++ src/main/java/com/example/Almoxarifado/controller/DiodoController.java
@@ -6,13 +6,19 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Almoxarifado.dto.AtualizarDiodoDTO;
 import com.example.Almoxarifado.model.Diodo;
 import com.example.Almoxarifado.repository.DiodoRepository;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@RestController
+@RequestMapping("/diodo")
 public class DiodoController {
     @Autowired
     private DiodoRepository repository;
