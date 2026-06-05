f9aeb0fa6c08841a1046c9b3a91c4492dcbf710f
JulianoMXM <juliano.massocalegal@gmail.com>
2026-06-05 00:47:01 -0300

    feat: Adiciona novas funções de busca para permitir filtros.

--- src/main/java/com/example/Almoxarifado/repository/CapacitorRepository.java
+++ src/main/java/com/example/Almoxarifado/repository/CapacitorRepository.java
@@ -1,9 +1,17 @@
 package com.example.Almoxarifado.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.Almoxarifado.common.enums.UnidadeDeMedidaEnum;
 import com.example.Almoxarifado.model.Capacitor;
 
 @Repository
-public interface CapacitorRepository extends JpaRepository<Capacitor, Long>{}
+public interface CapacitorRepository extends JpaRepository<Capacitor, Long>{
+    List<Capacitor> findByCapacitanciaBetweenAndUnidadeDeMedida(Double min, Double max, UnidadeDeMedidaEnum unidade);
+    List<Capacitor> findByCapacitanciaGreaterThanEqualAndUnidadeDeMedida(Double min, UnidadeDeMedidaEnum unidade);
+    List<Capacitor> findByCapacitanciaLessThanEqualAndUnidadeDeMedida(Double max, UnidadeDeMedidaEnum unidade);
+    List<Capacitor> findByUnidadeDeMedida(UnidadeDeMedidaEnum unidade);
+}
