e11396010de6d439ef4c1362eab7ec26ef41dd63
JulianoMXM <juliano.massocalegal@gmail.com>
2026-06-04 22:51:08 -0300

    refact: Remove importação não utilizada.

--- src/main/java/com/example/Almoxarifado/controller/ResistorController.java
+++ src/main/java/com/example/Almoxarifado/controller/ResistorController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Almoxarifado.dto.AtualizarResistorDTO;
-import com.example.Almoxarifado.model.Protoboard;
 import com.example.Almoxarifado.model.Resistor;
 import com.example.Almoxarifado.repository.ResistorRepository;
 
