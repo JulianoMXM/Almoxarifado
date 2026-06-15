e8499fa9412ff5ecd5a953ff346262948aefa285
JulianoMXM <juliano.massocalegal@gmail.com>
2026-06-14 23:21:44 -0300

    refact: Muda para notNull

--- src/main/java/com/example/Almoxarifado/model/Discente.java
+++ src/main/java/com/example/Almoxarifado/model/Discente.java
@@ -1,6 +1,6 @@
 package com.example.Almoxarifado.model;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Discente extends Pessoa {
-    @NotBlank(message = "O RA é obrigatório.")
+    @NotNull(message = "O RA é obrigatório.")
     @Size(min = 8, max = 8, message = "O RA deve conter 8 digitos.")
     // 8 digitos no ra pq estou levando em consideração que o sitema é para o nosso campus q o ra é 8 digitos.
     private Integer ra;
--- src/main/java/com/example/Almoxarifado/model/Docente.java
+++ src/main/java/com/example/Almoxarifado/model/Docente.java
@@ -1,7 +1,7 @@
 package com.example.Almoxarifado.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Docente extends Pessoa {
-    @NotBlank(message = "O SIAPE é obrigatório.")
+    @NotNull(message = "O SIAPE é obrigatório.")
     @Size(min = 7, max = 8, message = "O SIAPE deve conter entre 7 e 8 digitos.")
     private Integer siape;
 }
